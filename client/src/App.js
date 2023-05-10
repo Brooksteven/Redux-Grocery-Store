13489a68badb4859fcd31a0c44334b885ceb5ad4
Brooksteven <Brooksteven580@gmail.com>
2023-05-09 22:39:09 -0400

    finished client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,52 @@
-import React, { useEffect } from 'react';
-import CarComponent from './components/CarComponent';
-// Importing our theme provider which will make our global state available to child components
-import CarProvider from './utils/CarContext';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
-export default function App() {
-  useEffect(() => {
-    document.title = 'Module 22.1: Reducers';
-  }, []);
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import NoMatch from './pages/NoMatch';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Success from './pages/Success';
+import Nav from './components/Nav';
+import OrderHistory from './pages/OrderHistory';
+import { Provider } from 'react-redux';
+import store from './utils/store';
 
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+  uri: '/graphql',
+});
+
+function App() {
   return (
-    <CarProvider>
-      <CarComponent />
-    </CarProvider>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Provider store={store}>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" component={Success} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Provider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
+
+export default App;
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,27 +1,94 @@
-import createId from './createId';
+import {
+  UPDATE_PRODUCTS,
+  ADD_TO_CART,
+  UPDATE_CART_QUANTITY,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  CLEAR_CART,
+  TOGGLE_CART,
+} from './actions';
 
-export const reducer = (state, action) => {
+const initialState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: '',
+};
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_CAR': {
-      const newID = createId(state.cars);
-      const newCar = { ...action.payload, id: newID };
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
 
       return {
         ...state,
-        cars: [...state.cars, newCar],
+        cartOpen: newState.length > 0,
+        cart: newState,
       };
-    }
-    case 'REMOVE_CAR': {
+
+    case CLEAR_CART:
       return {
         ...state,
-        cars: state.cars.filter((car) => car.id !== action.payload),
+        cartOpen: false,
+        cart: [],
       };
-    }
-    default: {
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
+
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
+    default:
       return state;
-    }
   }
 };
 
-//The reducer must be a pure function.
-//
+export default reducer;
